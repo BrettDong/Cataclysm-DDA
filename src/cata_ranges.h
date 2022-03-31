aabecb9c62069ce43970ad943bf3253b256854ac
Binrui Dong <brett.browning.dong@gmail.com>
2022-03-31 17:03:06 +0800

    Add size(), empty(), front() helper functions to any_view

---
+++ src/cata_ranges.h
@@ -0,0 +1,39 @@
+#pragma once
+#ifndef CATA_SRC_CATA_RANGES_H
+#define CATA_SRC_CATA_RANGES_H
+
+#include <utility>
+
+#include <range/v3/view/any_view.hpp>
+
+namespace cata
+{
+namespace ranges
+{
+
+template<typename T>
+size_t size( ::ranges::any_view<T> &&view )
+{
+    size_t count = 0;
+    for( auto it : view ) {
+        ++count;
+    }
+    return count;
+}
+
+template<typename T>
+bool empty( ::ranges::any_view<T> &&view )
+{
+    return size( std::forward<::ranges::any_view<T>>( view ) ) == 0;
+}
+
+template<typename T>
+T front( ::ranges::any_view<T> &&view )
+{
+    return *view.begin();
+}
+
+} // namespace ranges
+} // namespace cata
+
+#endif // CATA_SRC_CATA_VIEWS_H
